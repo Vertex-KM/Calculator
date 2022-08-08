811d7220d243e53d2030e2f33b434fcf86afb8f3
Ibadullah Khallmohammad <Ibadullahkhall@gmail.com>
2022-08-08 13:47:18 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let container = document.getElementById("container");
+let row = document.getElementsByClassName("row");
+
+
+
+
+/*function operate () {
+   // num1 = Number(prompt("number one"));
+    //num2 = Number(prompt("number two"));
+    opera = addNums() //addEventListener(click, operaterBtn); 
+    let results = `${opera}`;
+}
+operate();
+*/
+function addNums () {
+    let a = num1;
+    let b = num2;
+    let addResults = a + b;
+    console.log(addResults);
+}
+
+function subtractNum (num1, num2) {
+    let a = num1;
+    let b = num2;
+    let subResults = a - b;
+    console.log(subResults);  
+}
+
+function multiplyNum (num1, num2) {
+    let a = num1;
+    let b = num2;
+    let multiResults = a * b;
+    console.log(multiResults); 
+}
+
+function divideNum (num1, num2) {
+    let a = num1;
+    let b = num2;
+    let divResults = a / b; 
+    console.log(divResults);
+}
+
+function getNumbers(){
+    addEventListener("click",)
+}
+
